3696b7a513bd3f15569f9a3326ae8325aa6e219f
LunarStorm97 <ian8388382@proton.me>
2025-01-24 19:38:46 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Parte de las combinaciones a procesar
-part=$1
-
 # Archivo de log
 log_file="firmware_versions_combinations.log"
 echo "Fecha de consulta: $(date)" > "$log_file"
@@ -18,36 +15,17 @@ generate_combinations() {
     done
 }
 
-# Total de combinaciones
-total_combinations=17576  # Total de combinaciones (26^3)
-
-# Número de combinaciones por parte (aproximadamente 150)
-combinations_per_part=150
-
-# Calcular el rango de combinaciones para esta parte
-start=$(((part - 1) * combinations_per_part))
-end=$((part * combinations_per_part - 1))
-
-# Asegurarse de que la última parte cubra hasta el final
-if [ "$part" -eq 117 ]; then
-    end=$((total_combinations - 1))
-fi
-
-# Generar y procesar las combinaciones correspondientes
-counter=0
+# Recorrer todas las combinaciones generadas
 for region in $(generate_combinations); do
-    if [[ $counter -ge $start && $counter -le $end ]]; then
-        echo "Consultando la región: $region"
+    echo "Consultando la región: $region"
 
-        # Ejecutar el comando curl para obtener la versión
-        version=$(curl -s "http://fota-cloud-dn.ospserver.net/firmware/$region/SM-A236M/version.xml" | grep -oP '(?<=<latest o="\d{02}">)[^/]+')
+    # Ejecutar el comando curl para obtener la versión
+    version=$(curl -s "http://fota-cloud-dn.ospserver.net/firmware/$region/SM-A236M/version.xml" | grep -oP '(?<=<latest o="\d{02}">)[^/]+')
 
-        # Si se encuentra una versión, agregarla al archivo de log
-        if [[ -n "$version" ]]; then
-            echo "Región: $region - Versión: $version" >> "$log_file"
-        fi
+    # Si se encuentra una versión, agregarla al archivo de log
+    if [[ -n "$version" ]]; then
+        echo "Región: $region - Versión: $version" >> "$log_file"
     fi
-    ((counter++))
 done
 
-echo "Consulta completada para la parte $part. Los resultados se han guardado en $log_file"
+echo "Consulta completada. Los resultados de las versiones encontradas se han guardado en $log_file"
